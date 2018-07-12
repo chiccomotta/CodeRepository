611fada9ca83395fa787c97dce591f215d2c68b5
Cristiano Motta <cristiano.motta@hotmail.it>
2018-07-12 14:31:18 +0200

    Filter attribute for json serialization

---
+++ ChosoJsonConverterAttribute.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repower.LeMA.API.Filters
+{
+    /// <summary>
+    /// Filtro per formattare il json di risposta secondo lo standard di Choso
+    /// </summary>
+    public class ChosoJsonConverterAttribute : TypeFilterAttribute
+    {
+        public ChosoJsonConverterAttribute() : base(typeof(ChosoJsonConverter))
+        {
+        }
+
+        public class ChosoJsonConverter : IAsyncResultFilter
+        {
+            private readonly HttpContext Context;
+
+            public ChosoJsonConverter(IHttpContextAccessor contextAccessor)
+            {
+                this.Context = contextAccessor.HttpContext;
+            }
+
+            public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+            {
+                dynamic originResult = context.Result;
+
+                // Se mi chiama Choso cambio name strategy
+                if (IsChosoCalling())
+                {
+                    JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings
+                    {
+                        Formatting = Formatting.Indented,
+                        TypeNameHandling = TypeNameHandling.None
+                    };
+
+                    var contractResolver = new DefaultContractResolver
+                    {
+                        NamingStrategy = new SnakeCaseNamingStrategy()
+                    };
+
+                    jsonSerializerSettings.ContractResolver = contractResolver;
+
+                    context.Result = new JsonResult(originResult.Value, jsonSerializerSettings);
+                }
+                else
+                {
+                    context.Result = new JsonResult(originResult.Value, Globals.JsonSerializerSettings);
+                }
+
+                await next();
+            }
+
+            protected bool IsChosoCalling()
+            {
+                var userAgent = Context.Request.Headers["User-Agent"].FirstOrDefault()?.ToLower();
+                if (string.IsNullOrEmpty(userAgent))
+                    return false;
+
+                return userAgent == "repower-choso";
+            }
+        }
+    }
+}
